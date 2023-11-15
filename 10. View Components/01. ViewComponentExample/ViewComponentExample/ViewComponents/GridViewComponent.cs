653e68cc51a01146f90c07338f5c98daafd01354
quocanh4433 <hoquocanh.td@gmail.com>
2023-11-16 00:34:55 +0700

    View Components

---
+++ 10. View Components/01. ViewComponentExample/ViewComponentExample/ViewComponents/GridViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewComponentsExample.ViewComponents
+{
+    public class GridViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            ////invoked a partial view Views/Shared/Components/Grid/Sample.cshtml
+            //return View("Sample");
+
+            //invoked a partial view Views/Shared/Components/Grid/Default.cshtml
+            return View();
+        }
+    }
+}
